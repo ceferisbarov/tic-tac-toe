0c2febedc6044e175cc824183f0522cb2ef7813d
ceferisbarov <60838378+ceferisbarov@users.noreply.github.com>
2020-02-28 22:15:07 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,17 +2,19 @@
 #include <cstdlib>
 void display (char[]);
 void play (int, int&, char[]);
-bool noWinner (void);
+bool noWinner(char a[]);
+
 int main()
 {
     char a[10]="123456789";
     display(a);                      //this is the playing board
     char player;
     int turn=0;
-    while (noWinner() && turn<9)
+    while (noWinner(a)
+           &&turn<9)
     play (player, turn, a);
 
-    if(noWinner())
+    if(noWinner(a))
     std::cout<<"No Winner.";//checks if there is no winner
     else if(turn%2==1)std::cout<<"Player 1, CONGRATULATIONS!"<<'\n';
     else std::cout<<"Player 2, CONGRATULATIONS"<<'\n';
@@ -22,12 +24,22 @@ int main()
 
 void play (int player, int& turn, char a[])
 {
-    std::cin>>player;             //the player looks at the board and inputs where (which number) he wants to cross out
+    replay: std::cin>>player;             //the player looks at the board and inputs where (which number) he wants to cross out
     turn++;                       //to keep record of the number of terms
     for(int i=0;i<9;i++)
     {
-        if((player==i+1)&&(turn%2==1))a[i]='x';
-        else if ((player==i+1)&&(turn)%2==0)a[i]='o';
+        if((player==i+1)&&(turn%2==1))
+        {
+            if(a[i]=='x' || a[i]=='o')
+            {
+                std::cout<<"Overwrite forbidden!";
+                turn--;
+                goto replay;
+            }
+            a[i]='x';
+        }
+        else if ((player==i+1)&&(turn)%2==0)
+            a[i]='o';
     }
     display (a);                     //shows the updated board
 }
@@ -39,14 +51,14 @@ void display (char a[])
              <<a[6]<<a[7]<<a[8]<<'\n';
 }
 
-bool noWinner (void)
+bool noWinner(char a[])
 {
-    return(!(a[0]==a[1]&&a[1]==a[2])
+    return (!(a[0]==a[1]&&a[1]==a[2])
             &&!(a[1]==a[4]&&a[4]==a[7])
             &&!(a[2]==a[5]&&a[5]==a[8])
             &&!(a[0]==a[4]&&a[4]==a[8])
             &&!(a[2]==a[4]&&a[4]==a[6])
             &&!(a[0]==a[3]&&a[3]==a[6])
             &&!(a[1]==a[4]&&a[4]==a[7])
-            &&!(a[2]==a[5]&&a[5]==a[8]))
+            &&!(a[2]==a[5]&&a[5]==a[8]));
 }
